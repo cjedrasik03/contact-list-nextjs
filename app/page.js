31baf9de9c81c6788630c83584aec2f717c5c833
chris jedrasik <chris.jed03@gmail.com>
2025-03-22 18:33:55 -0400

    designed home page contact cards and set button links to correct routes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
+
 const initialContacts = [
   {
     id: 123456,
@@ -9,9 +13,45 @@ const initialContacts = [
 ];
 
 export default function HomePage() {
-    return (
-    <div className="container mt-4">
-      <h1>My Contacts</h1>
-    </div>
+  const [contacts, setContacts] = useState(initialContacts);
+
+  return (
+    <>
+      <div className="container text-center mt-4">
+      <h1 className='fw-bold'>All Contacts</h1>
+
+      <Link href="/contacts/new" className="btn btn-primary fw-bold mb-3">
+        ADD CONTACT
+      </Link>
+
+      <div className='container fw-bold text-start'>
+      {contacts.map(contact => (
+        <div key={contact.id} className="card mb-3">
+          <div className="card-body d-flex align-items-center justify-content-between">
+            <div className="d-flex align-items-center gap-3">
+              <img
+                src={contact.image_url}
+                alt={contact.name}
+                width="80"
+                height="80"
+                className="rounded-circle object-fit-cover"
+              />
+              <div>
+                <h5 className="mb-1">
+                  <Link href={`/contacts/${contact.id}`}>{contact.name}</Link>
+                </h5>
+                <p className="mb-0">Email: {contact.email}</p>
+                <p className="mb-0">Phone: {contact.phone_number}</p>
+              </div>
+            </div>
+            <Link href={`/contacts/${contact.id}`} className="btn btn-outline-primary">
+              View Contact
+            </Link>
+          </div>
+        </div>
+      ))}
+      </div>
+      </div>
+    </>
   );
 }
